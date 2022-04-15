d4684b56eefb7eae4ace991e882ffff68d67e0d7
Ankush263 <ankushbanik263@gmail.com>
2022-04-15 11:40:32 +0530

    no need for useFindAndModify, false

--- server/index.js
+++ server/index.js
@@ -10,4 +10,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
 const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running to the port: ${PORT}`)))
+  .catch((err) => console.log(err.message))
+
+//mongoose.set('useFindAndModify', false)

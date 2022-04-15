07ef1d9259718375524cb640030a3a713be31c74
Ankush263 <ankushbanik263@gmail.com>
2022-04-15 12:24:38 +0530

    update index.js and post.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import postsRoute from './routes/posts.js'
+
 const app = express()
+
+app.use('/posts', postsRoute)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))

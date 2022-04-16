38de918228ef01cbbffedbdb9d3dc1aef4b81245
Ankush263 <ankushbanik263@gmail.com>
2022-04-16 09:42:08 +0530

    fix and update Post, Posts, Form components

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Post = () => {
+  return (
+    <h1>Post</h1>
+  )
+}
+
+export default Post
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Post from './Post/Post'
+
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post/>
+      <Post/>
+    </>
+  )
+}
+
+export default Posts

4374e9697c0ea08bb474be2aecd1156943d1387a
Ankush263 <ankushbanik263@gmail.com>
2022-04-16 09:49:42 +0530

    create stylesheet and add that with app components

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import memories from './images/memories.png'
+import useStyle from './styles'
 
 const App = () => {
+  const classes = useStyle()
+
   return (
     <Container maxidth='lg'>
-      <AppBar position="static" color='inherit'>
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt='memories' height='60'/>
+      <AppBar className={classes.appBar} position="static" color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={memories} alt='memories' height='60'/>
       </AppBar>
       <Grow in>
         <Container>

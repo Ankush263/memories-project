a4a0065913af05a2a4454f7da8d1003160529127
Ankush263 <ankushbanik263@gmail.com>
2022-04-16 12:53:32 +0530

    create and update posts.js

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,10 @@
+export default (posts = [], action) => {
+  switch(action.type) {
+    case 'FETCH_ALL':
+      return posts
+    case 'CREATE':
+      return posts
+    default:
+      return posts
+  }
+}

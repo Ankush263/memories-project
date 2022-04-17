7cb7f1b420422526c45b4ed7be2b8513b8afa29a
Ankush263 <ankushbanik263@gmail.com>
2022-04-17 09:58:02 +0530

    update post.js and package.json

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { useSelector } from "react-redux"
 import Post from './Post/Post'
 
 import useStyle from './styles'
 const Posts = () => {
+  const post = useSelector((state) => state.posts)
   const classes = useStyle()
+
+  console.log(posts)
   return (
     <>
       <h1>Posts</h1>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = [], action) => {
   switch(action.type) {
     case 'FETCH_ALL':
-      return posts
+      return action.payload
     case 'CREATE':
       return posts
     default:

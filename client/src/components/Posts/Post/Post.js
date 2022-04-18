623e2d66c1be5f422f610125ea88cf8f664f97ef
Ankush263 <ankushbanik263@gmail.com>
2022-04-18 11:33:29 +0530

    update Post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,8 @@
 import React from "react"
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core"
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAl'
+import DeleteIcon from "@material-ui/icons/Delete"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
 
 import useStyle from './styles'
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -15,7 +15,7 @@ const Posts = () => {
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {posts.map((post) => (
           <Grid key={post._id} item xs={12} sm={6}>
-
+            <Post post={post} />
           </Grid>
         ))}
       </Grid>

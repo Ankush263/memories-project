f9ad2f737671a9abf3468aae3ba0d1a35ae5534a
Ankush263 <ankushbanik263@gmail.com>
2022-04-19 13:37:40 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     dispatch(getPosts())
-  }, [dispatch])
+  }, [currentId, dispatch])
 
   return (
     <Container maxidth='lg'>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,6 +27,6 @@ export const updatePost = async (req, res) => {
 
   if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
   res.json(updatedPost)
 }

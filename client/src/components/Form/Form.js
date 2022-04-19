a793e780538402e49ea330050da79cde29e9c31b
Ankush263 <ankushbanik263@gmail.com>
2022-04-19 10:34:04 +0530

    update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,7 +7,7 @@ import useStyle from './styles'
 
 import { createPost } from "../../actions/posts"
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: '', title: '', message: '', tags: '', selectedFile: ''
   })

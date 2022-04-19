b5f343cdc5d84c3543dbed36d6f547e40da1ac3e
Ankush263 <ankushbanik263@gmail.com>
2022-04-19 10:02:04 +0530

    fix client side post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux"
 import Post from './Post/Post'
 
 import useStyle from './styles'
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts)
   const classes = useStyle()
 
@@ -14,8 +14,8 @@ const Posts = () => {
     !posts.length ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={6}>
-            <Post post={post} />
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
           </Grid>
         ))}
       </Grid>

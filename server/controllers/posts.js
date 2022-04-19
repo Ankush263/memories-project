f8fa3bff44cdd7f5764428c2b7c1055514a7b107
Ankush263 <ankushbanik263@gmail.com>
2022-04-19 09:29:58 +0530

    update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import PostMessage from "../modeles/postMessage.js"
 
 export const getPost = async (req, res) => {
@@ -18,4 +19,14 @@ export const createPost = async (req, res) => {
   } catch(err) {
     res.status(409).json({ message: err.message })
   }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params
+  const post = req.body
+
+  if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+  res.json(updatedPost)
 }

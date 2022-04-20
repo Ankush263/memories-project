2722c0aca09f82d4c51a0f12b033546b6ef3b4be
Ankush263 <ankushbanik263@gmail.com>
2022-04-20 09:31:32 +0530

    add deletePost method in posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
-import { getPost, createPost, updatePost } from '../controllers/posts.js'
+import { getPost, createPost, updatePost, deletePost } from '../controllers/posts.js'
 
 const router = express.Router()
 
 router.get('/', getPost)
 router.post('/', createPost)
 router.patch('/:id', updatePost)
+router.delete(':id', deletePost)
 
 export default router

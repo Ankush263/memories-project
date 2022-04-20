7cfb24449cc586d3fbafb26804ddff11a070bdd9
Ankush263 <ankushbanik263@gmail.com>
2022-04-20 09:40:46 +0530

    Add deletePost route in posts.js inside router

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -30,3 +30,13 @@ export const updatePost = async (req, res) => {
   const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
   res.json(updatedPost)
 }
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+  await PostMessage.findByIdAndRemove(id)
+
+  res.json({ message: 'post deleted successfully'})
+}

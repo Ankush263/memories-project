9f903447bcb030dcb1179ad71f5fbb453448ad35
Ankush263 <ankushbanik263@gmail.com>
2022-04-20 09:53:26 +0530

    Add deletePost method inside action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -36,3 +36,13 @@ export const updatePost = (id, post) => async(dispatch) => {
     console.log(error)
   }
 }
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id)
+
+    dispatch({ type: 'DELETE', payload: id})
+  } catch(err) {
+    console.log(err)
+  }
+}

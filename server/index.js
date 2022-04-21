20d507e3d86057f7907fb0f225c3f43c800edd54
Ankush263 <ankushbanik263@gmail.com>
2022-04-21 09:04:20 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,25 +1,23 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-import postsRoute from './routes/posts.js'
+import postRoutes from './routes/posts.js';
 
-const app = express()
+const app = express();
 
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
+app.use('/posts', postRoutes);
 
-app.use('/posts', postsRoute)
-
-// const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running to the port: ${PORT}`)))
-  .catch((err) => console.log(err))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-//mongoose.set('useFindAndModify', false)
+//mongoose.set('useFindAndModify', false);
--- server/modeles/postMessage.js
+++ server/modeles/postMessage.js
@@ -8,11 +8,11 @@ const postSchema = mongoose.Schema({
   selectedFile: String,
   likeCount: {
     type: Number,
-    default: 0
+    default: 0,
   },
   createdAt: {
     type: Date,
-    default: new Date()
+    default: new Date(),
   }
 })
 

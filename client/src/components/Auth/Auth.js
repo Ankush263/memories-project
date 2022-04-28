54eb8279c4b322a9b9c6e36b62dcbbb3da94337e
Ankush263 <ankushbanik263@gmail.com>
2022-04-28 09:38:54 +0530

    update Auth and create input.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
-
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import useStyles from './styles'
 
 const Auth = () => {
+  const classes = useStyles()
+
+  const isSignup = false
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
   return (
-    <div>
-      Auth
-    </div>
+    <Container component='main' maxWidth='xs'>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              isSignup && (
+                <>
+                  <TextField name='firstName' label='First Name' handleChange={handleChange} autoFocus xs={6} />
+                  <TextField name='lastName' label='First Name' handleChange={handleChange} autoFocus xs={6} />
+                </>
+              )
+            }
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 

bc91963937d6a93b2a41f6ae47cd6ae60608ddbb
Ankush263 <ankushbanik263@gmail.com>
2022-04-28 22:42:09 +0530

    update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,37 +1,33 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { getPosts } from '../../actions/posts';
+import { Container, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
-
-import Posts from './../Posts/Posts'
-import From from '../Form/Form'
+import { getPosts } from '../../actions/posts'
+import Posts from '../Posts/Posts'
+import Form from '../Form/Form'
 
 const Home = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-  //const classes = useStyles();
+  const [currentId, setCurrentId] = useState(0)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
+    dispatch(getPosts())
+  }, [currentId, dispatch])
 
   return (
-    <div>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <From currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
+    <Grow in>
+      <Container>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
           </Grid>
-        </Container>
-      </Grow>
-    </div>
-  )
-}
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
 
 export default Home

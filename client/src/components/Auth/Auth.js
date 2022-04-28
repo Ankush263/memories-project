e6ed64e3086d615e96cc22fb58a3acf94c609eb6
Ankush263 <ankushbanik263@gmail.com>
2022-04-28 09:08:40 +0530

    Set up styles.js and import it in Auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+
+import useStyles from './styles'
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div>
+      Auth
+    </div>
   )
 }
 

f2d99a36dfcaf572ce69d0d29205faf8f8fd07ef
Ankush263 <ankushbanik263@gmail.com>
2022-04-29 11:33:16 +0530

    use useDispatch in Auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
 import { GoogleLogin } from 'react-google-login'
+import { useDispatch } from 'react-redux'
 
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import useStyles from './styles'
@@ -11,6 +12,7 @@ const Auth = () => {
   const classes = useStyles()
   const [showPassword, setShowPassword] = useState(false)
   const [isSignup, setIsSignup] = useState(false)
+  const dispatch = useDispatch()
 
   const handleShowPassword = () => setShowPassword((prevState) => !prevState)
 
@@ -28,7 +30,14 @@ const Auth = () => {
   }
 
   const googleSuccess = async (res) => {
-    console.log(res)
+    const result = res?.profileObj
+    const token = res?.tokenId
+
+    try {
+      dispatch({ type: 'AUTH', data: { result, token } })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   const googleFailure = (err) => {

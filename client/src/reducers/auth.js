a1083c45cad0d328666a267a2e29ba4daab16d14
Ankush263 <ankushbanik263@gmail.com>
2022-04-29 11:33:32 +0530

    update auth.js

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,1 +1,14 @@
 import { AUTH, LOGOUT } from '../constants/actionTypes';
+
+const authReducer = (state = { authData: null }, action) => {
+  switch (action.type) {
+    case AUTH:
+      console.log(action?.data)
+      return state
+  
+    default:
+      return state
+  }
+}
+
+export default authReducer

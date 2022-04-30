51e017d7272f9e0b389c0d9dd81662b93f660bf4
Ankush263 <ankushbanik263@gmail.com>
2022-04-30 10:42:29 +0530

    update post.ja

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,12 +1,11 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
-
-import * as api from '../api/index.js';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes'
+import * as api from '../api/index.js'
 
 export const getPosts = () => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    const { data } = await api.fetchPosts()
 
-    dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: FETCH_ALL, payload: data })
   } catch (error) {
     console.log(error.message);
   }
@@ -14,19 +13,19 @@ export const getPosts = () => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
-    const { data } = await api.createPost(post);
+    const { data } = await api.createPost(post)
 
-    dispatch({ type: CREATE, payload: data });
+    dispatch({ type: CREATE, payload: data })
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
   }
 };
 
 export const updatePost = (id, post) => async (dispatch) => {
   try {
-    const { data } = await api.updatePost(id, post);
+    const { data } = await api.updatePost(id, post)
 
-    dispatch({ type: UPDATE, payload: data });
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error.message);
   }
@@ -34,20 +33,20 @@ export const updatePost = (id, post) => async (dispatch) => {
 
 export const likePost = (id) => async (dispatch) => {
   try {
-    const { data } = await api.likePost(id);
+    const { data } = await api.likePost(id)
 
-    dispatch({ type: LIKE, payload: data });
+    dispatch({ type: LIKE, payload: data })
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
   }
 };
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await api.deletePost(id);
+    await api.deletePost(id)
 
-    dispatch({ type: DELETE, payload: id });
+    dispatch({ type: DELETE, payload: id })
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message)
   }
 };

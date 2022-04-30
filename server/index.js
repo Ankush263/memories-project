6326fd43aa4d746146debbf12958a70b13408f5d
Ankush263 <ankushbanik263@gmail.com>
2022-04-30 10:58:17 +0530

    Add userRoute

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
-import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
-const app = express();
+const app = express()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors())
 
-app.use('/posts', postRoutes);
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
 
 app.get('/', (req, res) => {
   res.send("Hello Ankush")
@@ -20,10 +22,10 @@ app.get('/', (req, res) => {
 
 const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} did not connect`))
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)

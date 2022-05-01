4150a6cd18beb9bd37f3c8dc08f89652aec5e520
Ankush263 <ankushbanik263@gmail.com>
2022-05-01 10:18:46 +0530

    export signIn and signUp

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -10,5 +10,5 @@ export const likePost = (id) => API.patch(`/posts/${id}/likePost`)
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost)
 export const deletePost = (id) => API.delete(`/posts/${id}`)
 
-export const signIn = (formData) => API.post('/users/signin', formData)
-export const signUp = (formData) => API.post('/users/signup', formData)
+export const signIn = (formData) => API.post('/user/signin', formData)
+export const signUp = (formData) => API.post('/user/signup', formData)

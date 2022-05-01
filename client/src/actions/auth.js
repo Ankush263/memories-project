01ff3848f0f296e4b9da069162087f282572f120
Ankush263 <ankushbanik263@gmail.com>
2022-05-01 10:01:15 +0530

    dispatch signIn and signUp

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,6 +3,10 @@ import * as api from '../api/index.js'
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
+    const { data } = await api.signIn(formData)
+
+    dispatch({ type: AUTH, data })
+
     history.push('/')
   } catch (error) {
     console.log(error)
@@ -11,6 +15,10 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
   try {
+    const { data } = await api.signUp(formData)
+
+    dispatch({ type: AUTH, data })
+
     history.push('/')
   } catch (error) {
     console.log(error)

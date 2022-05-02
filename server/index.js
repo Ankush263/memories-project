38bfbe5e23912bdf67b3e33c59e3ba69bb088efe
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 21:39:14 +0530

    .

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import dotenv from 'dotenv'
+
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 
 const app = express()
-dotenv.config()
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 const CONNECTION_URL = "mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-let PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))

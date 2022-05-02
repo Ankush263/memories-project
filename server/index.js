f221ece0b2381609b82b6cb141bfdd6b4890776f
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 23:04:03 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/users.js";
 
 const app = express();
+dotenv.config()
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
   res.send("Hello Ankush Banik123")
 })
 
-const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

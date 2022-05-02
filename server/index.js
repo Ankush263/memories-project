aefd011c30c0c9dfcf2854515b3a1ac1061b01a5
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 15:52:06 +0530

    update

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT|| 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(process.env.PORT|| 5000, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`))
 
 mongoose.set('useFindAndModify', false)

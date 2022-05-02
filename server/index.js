1f89efeeaec10d31fd76fd71df82f35869b7e2d6
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 20:43:04 +0530

    .

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.send("Hello Ankush Banik123")
 })
 
-const CONNECTION_URL = process.env.CONNECTION_URL
+const CONNECTION_URL = "mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

e59e8597f10504c5a4312b585c687196cfd1bc03
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 10:35:33 +0530

    Add name to postMessage

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
+    name: String,
     creator: String,
     tags: [String],
     selectedFile: String,

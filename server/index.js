b660c5d916b852802139784265fd6591128572c0
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 22:29:07 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -1,30 +1,36 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-import postRoutes from './routes/posts.js'
-import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
-const app = express()
+const app = express();
 
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
-
-app.use('/posts', postRoutes)
-app.use('/user', userRoutes)
-
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 app.get('/', (req, res) => {
   res.send("Hello Ankush Banik123")
 })
 
-const URL = "mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const port = process.env.PORT || 5000
+const CONNECTION_URL = 'mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(port, () => console.log(`Server Running on Port: http://localhost:${port}`)))
-  .catch((error) => console.log(`${error} did not connect`))
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false)
+mongoose.set('useFindAndModify', false);
+
+
+
+
+
+
+
+// const URL = "mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"

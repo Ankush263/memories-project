06532e544a74979db3ad93e48152d15a10e3b2a2
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 15:58:00 +0530

    fix port

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 const CONNECTION_URL = process.env.CONNECTION_URL
-const PORT = process.env.PORT|| 5000
+//const PORT = process.env.PORT|| 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(process.env.PORT|| 5000, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))

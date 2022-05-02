cf22b6ed36152dbdf69d4ec0cbadb8cceeaa02c3
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 21:02:13 +0530

    update

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 })
 
 const CONNECTION_URL = "mongodb+srv://Ankush263:Ankush263@cluster0.3c3fb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT|| 5000
+const PORT = server.listen(process.env.PORT|| 5000)
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))

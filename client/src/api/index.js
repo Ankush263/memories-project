5f62130e60559c40a735f74f2be05e4f571f6ba8
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 10:29:29 +0530

    Add interceptors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,14 @@ const API = axios.create({ baseUrl: 'http://localhost:5000' })
 
 //const url = 'https://memories-project-263.herokuapp.com/posts'
 
+API.interceptors.request.use((req) => {
+  if(localStorage.getItem('profile')){
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+  }
+
+  return req
+})
+
 export const fetchPosts = () => API.get('/posts')
 export const createPost = (newPost) => API.post('/posts', newPost)
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`)

a1076315172fdbe190359b5f8d35927da6eed3ed
Ankush263 <ankushbanik263@gmail.com>
2022-05-02 12:16:55 +0530

    fix Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,52 +1,59 @@
-import React, { useState, useEffect} from 'react'
-import { Link, useHistory, useLocation } from 'react-router-dom'
-import { AppBar, Avatar, Button, Typography, Toolbar } from '@material-ui/core'
-import { useDispatch } from 'react-redux'
-import useStyles from './styles'
-import memories from '../../images/memories.png';
-const Navbar = () => {
-  const classes = useStyles()
+import React, { useState, useEffect } from 'react';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
-  const [ user, setUser ] = useState(JSON.parse(localStorage.getItem('profile')))
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const location = useLocation()
+import memories from '../../images/memories.png';
+import * as actionType from '../../constants/actionTypes';
+import useStyles from './styles';
+
+const Navbar = () => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
+  const classes = useStyles();
 
   const logout = () => {
-    dispatch({ type: 'LOGOUT' })
+    dispatch({ type: actionType.LOGOUT });
 
-    history.push('/')
+    history.push('/auth');
 
-    setUser(null)
-  }
+    setUser(null);
+  };
 
   useEffect(() => {
-    const token = user?.token
+    const token = user?.token;
 
-    setUser(JSON.parse(localStorage.getItem('profile')))
-  }, [location])
-  
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
+
   return (
-    <div>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <div className={classes.brandContainer}>
-          <Typography component={Link} to='/' className={classes.heading} variant="h2" align="center">Memories</Typography>
-          <img className={classes.image} src={memories} alt="icon" height="60" />
-        </div>
-        <Toolbar className={classes.toolbar}>
-          {user ? (
-            <div className={classes.profile}>
-              <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-              <Typography className={classes.userName} varient="h6">{user.result.name}</Typography>
-              <Button varient="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
-            </div>
-          ) : (
-            <Button component={Link} to='/auth' varient="contained" color="primary">Sign In</Button>
-          )}
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
-}
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user?.result ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
